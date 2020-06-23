1fd0ac19545517e96a2492d6488739aff3accf96
MukeshKumarSuman <mukesh060220@gmail.com>
2020-06-23 09:33:24 +0530

    server side java code

---
+++ backend-java-code/burger-builder/src/main/java/com/nps/controller/IngredientsController.java
@@ -0,0 +1,20 @@
+package com.nps.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nps.entity.Ingredients;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/burger")
+public class IngredientsController {
+
+	@GetMapping("/ingredients")
+	public ResponseEntity<Ingredients> getDefaultIngredients() {
+		return ResponseEntity.ok(new Ingredients());
+	}
+}
